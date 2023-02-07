e60c3d83ab09d05c85370146f4a585a6645301e2
Pavel <sivec_p@mail.ru>
2023-02-08 02:41:25 +0300

    Add TrianglePascal

--- TrianglePascal/Program.cs
+++ TrianglePascal/Program.cs
@@ -35,3 +35,33 @@ void PascalTriangle(int[,] arr)
 }
 
 PascalTriangle(GetArray());
+
+//Python для сравнения
+
+// 1 способ
+
+// n = int(input())
+// row = [1]
+// print(' ' * (n - 1), *row)
+// for i in range(n - 1):
+//     row = [sum(j) for j in zip(row + [0], [0] + row)]
+//     print(' ' * (n - 2 - i), *row)
+
+// 2 способ
+
+// n = int(input())
+// a = [[1 for j in range(i + 1)] for i in range(n)]
+// for i in range(1, len(a)):
+//     for j in range(1, i):
+//         a[i][j] = a[i - 1][j] + a[i - 1][j - 1]
+// for i, row in enumerate(a):
+//     print(' ' * (n - i), *row)
+
+// 3 способ
+
+// from math import factorial
+// n = int(input())
+// for i in range(n):
+//     for j in range(i + 1):
+//         print(int(factorial(i) / (factorial(j) * factorial(i-j))), end = ' ')
+//     print()
